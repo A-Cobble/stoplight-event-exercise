d42bbdb547da4052b013854b42d053ddc54ed899
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-06-24 15:28:21 -0700

    Added DOM event listeners and handelers to toggle the on/off state of three light bulbs. Added new DOM event listeners and handlers to log the mouse state of each button. Added one final DOM event listener and handler to log the state of each of three bulbs.

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById('stopButton');
+let stopLight = document.getElementById('stopLight');
+let slowButton = document.querySelector('#slowButton');
+let slowLight = document.querySelector('#slowLight');
+let goButton = document.querySelector('#goButton');
+let goLight = document.querySelector('#goLight');
+let buttons = document.querySelectorAll('.button');
+let controls = document.getElementById('controls');
 
-  // YOUR CODE HERE
-})();
+stopButton.addEventListener('click', () => {
+  stopLight.classList.toggle('stop')
+})
+slowButton.addEventListener('click', () => {
+  slowLight.classList.toggle('slow');
+})
+goButton.addEventListener('click', () => {
+  goLight.classList.toggle('go');
+})
+
+
+let enterButton = (event) => {
+  console.log(`Entered the ${event.target.textContent} button`)
+}
+let exitButton = (event) => {
+  console.log(`Left the ${event.target.textContent} button`)
+}
+for (let i =0; i< buttons.length;i++){
+buttons[i].addEventListener('mouseenter', enterButton);
+buttons[i].addEventListener('mouseleave', exitButton);
+}
+
+
+controls.addEventListener('click', (event) => {
+  if(event.target.id === 'stopButton' && stopLight.classList.contains('stop')){
+    console.log(event.target.textContent + ' bulb is on');
+  } 
+  else if(event.target.id === 'slowButton' && slowLight.classList.contains('slow')){
+    console.log(event.target.textContent + ' bulb is on');
+  }
+  else if(event.target.id === 'goButton' && goLight.classList.contains('go')){
+    console.log(event.target.textContent + ' bulb is on');
+  }else{
+    console.log(event.target.textContent + ' bulb is off');
+  }
+})
